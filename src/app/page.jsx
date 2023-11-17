aafa9e59289af8929a365d8d7f61d6b8f84979c7
LucasChanquia <lucaschanquia@gmail.com>
2023-11-17 15:02:51 -0300

    Correction

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,29 @@
+
 import { prisma } from "../libs/prisma";
 import TaskCard from "../components/Card/TaskCard";
 
+
 async function loadTask() {
   const data = await prisma.task.findMany();
-  console.log(data);
   return data;
 }
-export const dynamic = 'force-dynamic'
-async function HomePage({params}) {
+
+
+async function HomePage ({params}) {
   const task = await loadTask();
+
   return (
     <>
-       <h1>Organizador de tareas Abuelo</h1>
+      <h1>Organizador de tareas Abuelo</h1>
       <section className="container mx-auto">
         <div className="grid grid-cols-3 gap-3 w-auto h-auto">
           {task?.map((elem) => (
-           <TaskCard elem={elem} key={elem.id} params={params} />
+            <TaskCard elem={elem} key={elem.id} params={params} />
           ))}
         </div>
       </section>
     </>
   );
-}
+          }
 
 export default HomePage;
